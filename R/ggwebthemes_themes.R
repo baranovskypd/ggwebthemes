8d3f053722376ebae67a47a783cc26bec072dfe6
Petr Baranovskiy <peterbar@protonmail.com>
2020-09-01 18:57:20 -0600

    v.0.0.0.9000 - first proper build

--- R/ggwebthemes_themes.R
+++ R/ggwebthemes_themes.R
@@ -3,8 +3,6 @@
 #' Custom ggplot2 theme for blogs (and similar online usage).
 #'
 #' This theme can be customized using the same arguments as ggplot2::theme.
-#' 
-#' @author Petr Baranovskiy
 #'
 #' @importFrom ggplot2 theme_bw
 #' @importFrom ggplot2 theme
@@ -12,30 +10,34 @@
 #' @export
 #'
 #' @examples
-#' palmerpenguins::penguins %>% 
-#'   group_by(species) %>% 
+#' \dontrun{
+#' library("dplyr")
+#' library("palmerpenguins")
+#' palmerpenguins::penguins %>%
+#'   group_by(species) %>%
 #'   ggplot(aes(x = bill_length_mm, y = body_mass_g)) +
 #'   facet_wrap(~ species, scales = "free_x") +
 #'   geom_point(size = 2, na.rm = TRUE) +
-#'   geom_smooth(formula = y ~ x, 
-#'               method = "lm", 
+#'   geom_smooth(formula = y ~ x,
+#'               method = "lm",
 #'               se = FALSE,
-#'               na.rm = TRUE) + 
+#'               na.rm = TRUE) +
 #'   theme_web_bw() +
 #'   labs(x = "Bill length, mm",
 #'        y = "Body mass, grams",
 #'        title = "Body Mass to Bill Length in Adelie, Chinstrap, and Gentoo Penguins",
 #'        subtitle = "Grouped by Species",
 #'        caption = "Data: Gorman, Williams, and Fraser 2014")
+#' }
 theme_web_bw <- function() {
-  theme_bw() + 
+  theme_bw() +
     theme(plot.title = element_text(size = 15, face = "bold",
                                     hjust = .5,
                                     margin = margin(t = 5, b = 15)),
-          plot.subtitle = element_text(size = 14, 
+          plot.subtitle = element_text(size = 14,
                                        hjust = .5,
                                        margin = margin(b = 15)),
-          plot.caption = element_text(size = 12, hjust = 0, 
+          plot.caption = element_text(size = 12, hjust = 0,
                                       margin = margin(t = 15)),
           panel.grid.major = element_line(colour = "grey88"),
           panel.grid.minor = element_blank(),
@@ -52,8 +54,6 @@ theme_web_bw <- function() {
 #' Custom ggplot2 theme for blogs (and similar online usage).
 #'
 #' This theme can be customized using the same arguments as ggplot2::theme.
-#' 
-#' @author Petr Baranovskiy
 #'
 #' @importFrom ggplot2 theme_classic
 #' @importFrom ggplot2 theme
@@ -61,30 +61,34 @@ theme_web_bw <- function() {
 #' @export
 #'
 #' @examples
-#' palmerpenguins::penguins %>% 
-#'   group_by(species) %>% 
+#' \dontrun{
+#' library("dplyr")
+#' library("palmerpenguins")
+#' palmerpenguins::penguins %>%
+#'   group_by(species) %>%
 #'   ggplot(aes(x = bill_length_mm, y = body_mass_g)) +
 #'   facet_wrap(~ species, scales = "free_x") +
 #'   geom_point(size = 2, na.rm = TRUE) +
-#'   geom_smooth(formula = y ~ x, 
-#'               method = "lm", 
+#'   geom_smooth(formula = y ~ x,
+#'               method = "lm",
 #'               se = FALSE,
-#'               na.rm = TRUE) + 
+#'               na.rm = TRUE) +
 #'   theme_web_classic() +
 #'   labs(x = "Bill length, mm",
 #'        y = "Body mass, grams",
 #'        title = "Body Mass to Bill Length in Adelie, Chinstrap, and Gentoo Penguins",
 #'        subtitle = "Grouped by Species",
 #'        caption = "Data: Gorman, Williams, and Fraser 2014")
+#' }
 theme_web_classic <- function() {
-  theme_classic() + 
+  theme_classic() +
     theme(plot.title = element_text(size = 15, face = "bold",
                                     hjust = .5,
                                     margin = margin(t = 5, b = 15)),
-          plot.subtitle = element_text(size = 14, 
+          plot.subtitle = element_text(size = 14,
                                        hjust = .5,
                                        margin = margin(b = 15)),
-          plot.caption = element_text(size = 12, hjust = 0, 
+          plot.caption = element_text(size = 12, hjust = 0,
                                       margin = margin(t = 15)),
           legend.title = element_text(size = 14, face = "bold"),
           legend.text = element_text(size = 14),
@@ -99,8 +103,6 @@ theme_web_classic <- function() {
 #' Custom ggplot2 theme for blogs (and similar online usage).
 #'
 #' This theme can be customized using the same arguments as ggplot2::theme.
-#' 
-#' @author Petr Baranovskiy
 #'
 #' @importFrom ggplot2 theme_void
 #' @importFrom ggplot2 theme
@@ -108,30 +110,34 @@ theme_web_classic <- function() {
 #' @export
 #'
 #' @examples
-#' palmerpenguins::penguins %>% 
-#'   group_by(species) %>% 
+#' \dontrun{
+#' library("dplyr")
+#' library("palmerpenguins")
+#' palmerpenguins::penguins %>%
+#'   group_by(species) %>%
 #'   ggplot(aes(x = bill_length_mm, y = body_mass_g)) +
 #'   facet_wrap(~ species, scales = "free_x") +
 #'   geom_point(size = 2, na.rm = TRUE) +
 #'   geom_smooth(formula = y ~ x, # optional: removes message
-#'               method = "lm", 
+#'               method = "lm",
 #'               se = FALSE,
-#'               na.rm = TRUE) + 
+#'               na.rm = TRUE) +
 #'   theme_web_void() +
 #'   labs(x = "Bill length, mm",
 #'        y = "Body mass, grams",
 #'        title = "Body Mass to Bill Length in Adelie, Chinstrap, and Gentoo Penguins",
 #'        subtitle = "Grouped by Species",
 #'        caption = "Data: Gorman, Williams, and Fraser 2014")
+#' }
 theme_web_void <- function() {
-  theme_void() + 
+  theme_void() +
     theme(plot.title = element_text(size = 15, face = "bold",
                                     hjust = .5,
                                     margin = margin(t = 5, b = 15)),
-          plot.subtitle = element_text(size = 14, 
+          plot.subtitle = element_text(size = 14,
                                        hjust = .5,
                                        margin = margin(b = 15)),
-          plot.caption = element_text(size = 12, hjust = 0, 
+          plot.caption = element_text(size = 12, hjust = 0,
                                       margin = margin(t = 15)),
           legend.title = element_text(size = 14, face = "bold"),
           legend.text = element_text(size = 14),
