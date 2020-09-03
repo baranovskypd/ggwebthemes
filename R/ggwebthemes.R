81b9e96cc108a441c2fc3f2f3ffd52e26fbe37a3
Petr Baranovskiy <peterbar@protonmail.com>
2020-09-02 18:16:38 -0600

    v.0.0.0.9200 - Improved description, dropped duplicate description file.

--- R/ggwebthemes.R
+++
@@ -1,9 +0,0 @@
-#' Some ggplot2 themes optimized for blogs.
-#'
-#' Some ggplot2 themes optimized for blogs and similar online usage.
-#'
-#' @author Petr Baranovskiy \email{peterbar@@protonmail.com} \url{https://dataenthusiast.ca/}
-#' @docType package
-#' @name ggwebthemes
-#' @seealso \url{https://ggplot2.tidyverse.org/reference/ggtheme.html}
-"_PACKAGE"
